10fa9f69b0ed8600fb49f6d80ad8075d4473f5e4
Guilherme Fogo <gui.fogo17@gmail.com>
2017-12-04 10:59:06 -0200

    Arquivo atualizado
    Ajuda nas Permissoes basicas do Android Nativo

--- Android_permissoes.java
+++ Android_permissoes.java
@@ -2,18 +2,20 @@ package testar.com.br.projet_teste;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 
-/**
- * Created by Pichau on 25/09/2017.
- */
+import java.io.File;
 
 public class Android_permissoes {
+    // atributo
     private Activity activity;
 
+    // get set
     public Activity getActivity() {
         return activity;
     }
@@ -27,6 +29,7 @@ public class Android_permissoes {
         setActivity(activity);
     }
 
+    // metodos diversos
     public void telefonar(Object numero){
         // verificar se tem a permissao no manifast com o pacote de permissoes permitidas
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE)
@@ -57,13 +60,47 @@ public class Android_permissoes {
         }
     }
 
-    public void Camera(Object foto){
+    public void Camera(Object caminho_foto){
+        // fazendo a verificação da permissao da camera
         if (ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(getActivity(),
                     new String[]{Manifest.permission.CAMERA},3);
-        }else{
-            // ação da camera
+        }else {
+            // fazendo a camera abrir:
+            // criando uma intent com o parametro basico do android para salvar a imagem num unico lugar
+            Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            // caminho da foto e o nome que ela terá
+            caminho_foto = getActivity().getExternalFilesDir(null + "/" + System.currentTimeMillis() + ".jpg");
+            // criado um arquivo de foto
+            File arquivo_foto = new File(caminho_foto.toString());
+            // defeindo a chave padrao de imagens do android (MediaStord) e o arquivo
+            camera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivo_foto));
+            // inicializando a camera
+            getActivity().startActivity(camera);
+
+        }
+    }
+
+    public void Internet(){
+        if (ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.INTERNET},4);
+        }else {
+            AlertDialog.Builder alerta = new AlertDialog.Builder(getActivity());
+            alerta.setMessage("Internet ativada").setTitle("Aviso").show();
+        }
+    }
+
+    public  void  localizacao(){
+        if (ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.LOCATION_HARDWARE},5);
+        }else {
+            AlertDialog.Builder alerta = new AlertDialog.Builder(getActivity());
+            alerta.setMessage("Localização OK").setTitle("Aviso").show();
         }
     }
 }
